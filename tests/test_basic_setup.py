801d28904a72bb8703753d67adefab9bf6205a98
mtkappen <mtkappen@gmail.com>
2026-07-18 18:19:34 -0400

    fix: Add @pytest.mark.django_db markers to database-dependent tests in test_basic_setup.py

--- tests/test_basic_setup.py
+++ tests/test_basic_setup.py
@@ -17,6 +17,7 @@ class TestInfrastructure:
         assert settings.DATABASES is not None
         assert 'default' in settings.DATABASES
 
+    @pytest.mark.django_db
     def test_db_fixture_works(self, db_setup):
         """Test that the database fixture creates data correctly."""
         # Check users were created
@@ -28,7 +29,7 @@ class TestInfrastructure:
         
         # Check character was created
         assert db_setup['character'].name == 'Test Character'
-
+    @pytest.mark.django_db
     def test_client_auth_works(self, client_auth, db_setup):
         """Test that authenticated client is working."""
         from django.urls import reverse
@@ -39,6 +40,7 @@ class TestInfrastructure:
         # Should be able to access (either 200 or redirect)
         assert response.status_code in [200, 301, 302]
 
+    @pytest.mark.django_db
     def test_admin_client_works(self, admin_client):
         """Test that admin client is working."""
         from django.urls import reverse
@@ -60,3 +62,5 @@ class TestInfrastructure:
         assert User is not None
         assert Campaign is not None
         assert Character is not None
+
+
